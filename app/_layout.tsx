d72f2aed9d3bb1aa661177a2168d8b5cb1de6d30
paingpyaethu <paingpyaethu@gmail.com>
2024-07-15 22:45:40 +0630

    OTP auth with clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,50 +1,133 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter, useSegments } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { Ionicons } from '@expo/vector-icons';
 import { useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
 import 'react-native-reanimated';
+import Colors from '@/constants/Colors';
+import { StatusBar } from 'expo-status-bar';
+import * as SecureStore from 'expo-secure-store';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error('Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
+}
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+	// Catch any errors thrown by the Layout component.
+	ErrorBoundary,
 } from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+const InitialLayout = () => {
+	const [loaded, error] = useFonts({
+		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+		...FontAwesome.font,
+	});
+	const router = useRouter();
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+	// Expo Router uses Error Boundaries to catch errors in the navigation tree.
+	useEffect(() => {
+		if (error) throw error;
+	}, [error]);
+
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
+
   useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    console.log('isSignedIn', isSignedIn)
+    // if (!isLoaded) return;
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+    // const inAuthGroup = segments[0] === '(authenticated)';
 
-  if (!loaded) {
-    return null;
-  }
+    // if (isSignedIn && !inAuthGroup) {
+    //   router.replace('/(authenticated)/(tabs)/home');
+    // } else if (!isSignedIn) {
+    //   router.replace('/');
+    // }
+  }, [isSignedIn]);
 
-  return <RootLayoutNav />;
-}
+	if (!loaded) {
+		return null;
+	}
 
-function RootLayoutNav() {
+	return (
+		<Stack>
+			<Stack.Screen name='index' options={{ headerShown: false }} />
+			<Stack.Screen
+				name='signup'
+				options={{
+					title: '',
+					headerBackTitle: '',
+					headerShadowVisible: false,
+					headerStyle: { backgroundColor: Colors.background },
+					headerLeft: () => (
+						<TouchableOpacity onPress={router.back}>
+							<Ionicons name='arrow-back' size={34} color={Colors.dark} />
+						</TouchableOpacity>
+					),
+				}}
+			/>
+      <Stack.Screen
+				name='login'
+				options={{
+					title: '',
+					headerBackTitle: '',
+					headerShadowVisible: false,
+					headerStyle: { backgroundColor: Colors.background },
+					headerLeft: () => (
+						<TouchableOpacity onPress={router.back}>
+							<Ionicons name='arrow-back' size={34} color={Colors.dark} />
+						</TouchableOpacity>
+					),
+				}}
+			/>
+		</Stack>
+	);
+};
 
-  return (
-    <Stack></Stack>
-  );
-}
+const RootLayoutNav = () => {
+	return (
+		<ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      {/* <QueryClientProvider client={queryClient}> */}
+        {/* <UserInactivityProvider> */}
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <StatusBar style="light" />
+            <InitialLayout />
+          </GestureHandlerRootView>
+        {/* </UserInactivityProvider> */}
+      {/* </QueryClientProvider> */}
+    </ClerkProvider>
+	);
+};
+
+export default RootLayoutNav;
